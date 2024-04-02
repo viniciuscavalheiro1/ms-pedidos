910d873105e96a033ece24c379930371215652e2
viniciuscavalheiro1 <viniciuspbu@gmail.com>
2024-04-02 01:13:47 -0300

    add controllers, repositorie

---
+++ src/main/java/ia/gobots/mspedidos/controllers/AuthenticationController.java
@@ -0,0 +1,51 @@
+package ia.gobots.mspedidos.controllers;
+
+import ia.gobots.mspedidos.domain.user.AuthenticationDTO;
+import ia.gobots.mspedidos.domain.user.LoginResponseDTO;
+import ia.gobots.mspedidos.domain.user.RegisterDTO;
+import ia.gobots.mspedidos.domain.user.User;
+import ia.gobots.mspedidos.security.TokenService;
+import ia.gobots.mspedidos.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
